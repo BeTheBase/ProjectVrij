2d65b01a2ff117d8cf2246bcc8359c75ebcdabb3
Bas <Bas.ghosts@gmail.com>
2019-04-24 15:24:33 +0200

    UnityCommit

---
+++ Unity_Project_Vrij/Assets/_Scripts/Tower.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Towers
+{
+    public class Tower : TowerBase
+    {
+        private float attackPower;
+        private float attackRange;
+        private float costAmount;
+        private GameObject towerPrefab;
+
+        public GameObject TowerProjectile;
+
+        private void Awake()
+        {
+            attackPower = AttackPower;
+            attackRange = AttackRange;
+            costAmount = CostAmount;
+            towerPrefab = TowerPrefab;
+        }
+
+        public void FireProjectile()
+        {
+            Instantiate(TowerProjectile, Vector3.forward, Quaternion.identity);
+
+        }
+
+
+    }
+}
---
+++ Unity_Project_Vrij/Assets/_Scripts/TowerBase.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Towers
+{
+    public abstract class TowerBase : MonoBehaviour
+    {
+        public int AttackPower { get; set; }
+        public int AttackRange { get; set; }
+        public int CostAmount { get; set; }
+        public GameObject TowerPrefab { get; set; }
+
+        public void Init()
+        {
+
+        }
+    }
+}
