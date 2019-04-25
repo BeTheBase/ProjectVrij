055426e8e3f3ad88fecc2f219e648df7df6c5592
Bas <Bas.ghosts@gmail.com>
2019-04-25 10:34:41 +0200

    TowerUpgrade and canvas

---
+++ Unity_Project_Vrij/Assets/_Scripts/SelectTowerManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectTowerManager : MonoBehaviour
+{
+    public GameObject ArcherTowerPrefab;
+    public GameObject BombTowerPrefab;
+    public GameObject MagicTowerPrefab;
+
+    public void SetArcherTower()
+    {
+        Instantiate(ArcherTowerPrefab, transform.position, transform.rotation);
+        this.gameObject.SetActive(false);
+    }
+
+    public void SetBombTower()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    public void SetMagicTower()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
